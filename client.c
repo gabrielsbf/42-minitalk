83d1370e1d89fd46d5de25715580ee4322ee979a
gabrfern <gsbfernandes@gmail.com>
2024-03-08 19:02:38 -0300

    client and server modifications

--- client.c
+++ client.c
@@ -6,23 +6,26 @@ void	to_bin(char *pid, char *message)
 {
 	int	pid_n;
 	int	i;
-	char c;
+	int c;
 
-	c = message[0];
+	c = 0;
 	i = 7;
 	pid_n = ft_atoi(pid);
 
-	while(i >= 0 )
+	while(message[c] != '\0')
 	{
-		if ((c >> i) & 1 )
-			kill(pid_n, SIGUSR1);
-		else
-			kill(pid_n, SIGUSR2);
-		ft_printf("processing %d\n", (c >> i & 1));
-		i--;
-		usleep(500);
+		while(i >= 0 )
+		{
+			if ((message[c] >> i) & 1 )
+				kill(pid_n, SIGUSR1);
+			else
+				kill(pid_n, SIGUSR2);
+			i--;
+			usleep(250);
+		}
+		i = 7;
+		c++;
 	}
-
 }
 
 int	main(int argc, char *argv[])
--- server.c
+++ server.c
@@ -4,7 +4,7 @@
 
 
 void sig_handler(int signum)
-  {
+{
 	static int	oct;
 	static char	bin;
 
@@ -17,32 +17,25 @@ void sig_handler(int signum)
 	oct++;
 	if (oct == 8)
 	{
-		ft_printf("response %c\n", bin);
+		ft_printf("%c", bin);
 		bin = 0;
 		oct = 0;
 	}
-	ft_printf("bin is %d - oct is : %d\n", bin, oct);
-  }
+}
 
 
 int main( void )
   {
 
 	struct sigaction sa;
-	// sigset_t set;
-
-	// sigemptyset( &set );
-	// sigaddset( &set, SIGUSR1);
 
 	sa.sa_flags = SA_SIGINFO | SA_RESTART;
 	sa.sa_handler = sig_handler;
 	ft_printf("server initialized! pid: %d\n", getpid());
 	sigaction(SIGUSR2, &sa, NULL);
 	sigaction(SIGUSR1, &sa, NULL);
-	
+
 	while(1)
 		pause();
-
-	/* Program will terminate with a SIGUSR2 */
   }
 
